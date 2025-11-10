26f58da499305cabe3c839b594c3702d86d7c68a
UihwanLee <luh5063014@naver.com>
2025-11-10 11:12:08 +0900

    feat: 마우스 회전에 따른 카메라 회전 구현

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,16 +10,34 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float jumpPower;       // 점프 파워
     private Vector2 curMoveVector;                  // 현재 움직임
 
+    [Header("CameraLook")]
+    [SerializeField] private float minXLook;            
+    [SerializeField] private float maxXLook;
+    [SerializeField] private float lookSensitivity;
+
+    private float curXLook;
+    private Vector2 mouseDelta;
+
+    [Header("LayerMaske")]
+    [SerializeField] private LayerMask groundMask;  // Ground Layer
+
     private Rigidbody _rb;
+    private Camera camera;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+        camera = Camera.main;
     }
 
     private void FixedUpdate()
     {
         Move();
+    }
+
+    private void LateUpdate()
+    {
+        CamerLook();
     }
 
     public void InputMove(InputAction.CallbackContext context)
@@ -55,6 +73,30 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        _rb.AddForce(Vector2.up * jumpPower, ForceMode.Impulse);
+        if(IsGrounded())
+        {
+            _rb.AddForce(Vector2.up * jumpPower, ForceMode.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        Ray ray = new Ray(transform.position, Vector2.down);
+        return Physics.Raycast(ray, 1.5f, groundMask);
+    }
+
+    public void InputMouseDelta(InputAction.CallbackContext context)
+    {
+        mouseDelta = context.ReadValue<Vector2>();
+    }
+    
+    private void CamerLook()
+    {
+        // 마우스 회전에 따른 카메라 이동
+        curXLook += mouseDelta.y * lookSensitivity;
+        curXLook = Mathf.Clamp(curXLook, minXLook, maxXLook);
+        camera.transform.localEulerAngles = new Vector3(-curXLook, 0f, 0f);
+
+        transform.eulerAngles += new Vector3(0f, mouseDelta.x * lookSensitivity, 0f);
     }
 }
